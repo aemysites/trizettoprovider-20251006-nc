6792669ef2369bac5cae7c7e00352e55bbac0288
Alena Vershinina <74716234+alenavershinina@users.noreply.github.com>
2025-10-13 08:47:18 +0200

    Update inject.js

--- tools/importer/transformers/inject.js
+++ tools/importer/transformers/inject.js
@@ -1,12 +1,14 @@
 (() => {
-  const wrapperParent = document.querySelector('.wpb_wrapper');
-if (wrapperParent) {
-  const cardRows = wrapperParent.querySelectorAll(':scope > div:not(:first-child):not(.vc_empty_space)');
-  if (cardRows.length) {
-    const wrapper = document.createElement('div');
-    wrapper.classList.add('blu-det-container');
-    wrapperParent.insertBefore(wrapper, cardRows[0]);
-    cardRows.forEach(div => wrapper.appendChild(div));
-  }
-}
+  const wrapperParents = document.querySelectorAll('.wpb_wrapper:has(.vc_row .wpb_wrapper a[href$=".pdf"])');
+
+  wrapperParents.forEach(wrapperParent => {
+    const cardRows = wrapperParent.querySelectorAll(':scope > div:has(a[href$=".pdf"])');
+  
+    if (cardRows.length) {
+      const wrapper = document.createElement('div');
+      wrapper.classList.add('blu-det-container');
+      wrapperParent.insertBefore(wrapper, cardRows[0]);
+      cardRows.forEach(div => wrapper.appendChild(div));
+    }
+  });
 })();
